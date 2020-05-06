ec8b2e19d59da2a0292acae8ddf31fed6215dc69
Rafael <arvenegasd@gmail.com>
2020-05-07 00:30:44 +0200

    getting data from github api

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,6 +1,9 @@
 import { getUsers } from "./services/fetch.js";
 
 const button = document.getElementById('search');
+
 button.addEventListener("click", () =>{
-    getUsers();
+    const keyword = document.getElementById('keyword').value;
+    getUsers(keyword);
+    event.preventDefault();
 })
--- app/src/js/services/fetch.js
+++ app/src/js/services/fetch.js
@@ -1,3 +1,14 @@
-export function getUsers(){
-    console.log("github");
+
+export function getUsers(username){
+    const url = 'https://api.github.com/users/'+username
+    fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log('There was a problem with the Fetch request:' + error.message);
+    });
 } 
