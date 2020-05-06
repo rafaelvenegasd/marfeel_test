0a07e6e07636b3b07084c3405c9f3df1f81af735
Rafael <arvenegasd@gmail.com>
2020-05-06 23:22:13 +0200

    main structure of the folders & node server for heroku

---
+++ app/test/test.js
@@ -0,0 +1,4 @@
+// Testing if data exist
+
+
+// testear snapshoot de que el contenido del JS sea lo que espero que pinte (HTML y DOM)
---
+++ server.js
@@ -0,0 +1,7 @@
+var static = require('node-static');
+var file = new static.Server('./app/src');
+require('http').createServer(function(request, response) {
+    request.addListener('end', function() {
+      file.serve(request, response);
+    }).resume();
+}).listen(process.env.PORT || 3000);
